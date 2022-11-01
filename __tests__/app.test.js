568aa94cdae6ec1cf2a619128221526fc92ef462
Connor McCue <connor.mccue@comcast.net>
2022-11-01 14:06:41 -0700

    boilerplate app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,23 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('test /zodiac route', () => {
+  it('zodiac route should return list of links to each zodiac sign', async () => {
+    const resp = await request(app).get('/zodiac');
+    expect(resp.text).toEqual('libra');
+  });
+});
+
+describe('test /zodiac/:id route', () => {
+  it('zodiac/:id route should return page about specific zodiac sign per id', async () => {
+    const resp = await request(app).get('/zodiac:id');
+    expect(resp.text).toEqual('definition');
+  });
+});
+
+describe('test horoscopes/:sign route', () => {
+  it('horoscopes/:sign should return page about specific horoscope of that day given the sign chosen', async () => {
+    const resp = await request(app).get('/horoscope/:sign');
+    expect(resp.text).toEqual('horoscope');
   });
 });
