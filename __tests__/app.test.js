99162c968de1466e6f43167694b9b52955c77a7a
Connor McCue <connor.mccue@comcast.net>
2022-11-01 15:42:15 -0700

    test 3 finished

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,10 +23,15 @@ describe('test /zodiac/:id route', () => {
   });
 });
 
-// describe('test horoscopes/:sign route', () => {
-//   it('horoscopes/:sign should return page about specific horoscope of that day given the sign chosen', async () => {
-//     const resp = await request(app).get('/horoscope/:sign');
-//     expect(resp.text).toEqual('horoscope');
-//   });
-// });
+describe('test horoscopes/:sign route', () => {
+  it('horoscopes/:sign should return page about specific horoscope of that day given the sign chosen', async () => {
+    const resp = await request(app).get('/horoscope/1');
+    const testHoroscopeOne = {
+      id: '1',
+      name: 'aquarius',
+      horoscope: 'You may have a hard time appreciating the little things this morning, dear Aquarius, as the moon forms a harsh square with the nodes of fate. Certain lifestyle luxuries, personal success, and status may not taste as sweet as you\'d hoped, causing you to close off emotionally. Don\'t be hard on yourself if you\'re feeling disenchanted with the world, but try to reach for that which brings you joy. The vibe will be charged within your domestic affairs when Luna squares off with Uranus this afternoon, so you\'ll want to be on guard for finicky appliances or temperamental roommates.'
+    };
+    expect(resp.body).toEqual(testHoroscopeOne);
+  });
+});
 
